cda5acbe15afe7b0559e2d5dbe5b11d8db65a5bf
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2019-12-04 18:13:59 -0300

    add o Form com InputText e Button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { View } from "react-native";
 
-// import { Container } from './styles';
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { Container, Form, Input, SubmitButton } from "./styles";
+
+Icon.loadFont();
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCaptalize={false}
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
