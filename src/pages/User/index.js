8157d70b4b5ee9fa315a790d30f248b4ff4877a3
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2019-12-05 12:56:42 -0300

    corrigido erro na  navegação

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -4,7 +4,7 @@ import { View } from "react-native";
 // import { Container } from './styles';
 
 export default function User({ navigation }) {
-  console.tron.log(navigation.getParams("user"));
+  console.tron.log(navigation.getParam("user"));
 
   return <View />;
 }
