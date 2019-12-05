245d214d096ed64ba9a87f241ae2ae379f727f7f
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2019-12-04 22:03:39 -0300

    mostrando o Usuários na lista e criado loading no button de adicionar um usuário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
-import { Keyboard } from "react-native";
+import { Keyboard, ActivityIndicator } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import api from "../../services/api";
 
-import { Container, Form, Input, SubmitButton } from "./styles";
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText
+} from "./styles";
 
 Icon.loadFont();
 
 export default class Main extends Component {
   state = {
     newUser: "",
-    users: []
+    users: [],
+    loading: false
   };
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -27,14 +42,15 @@ export default class Main extends Component {
 
     this.setState({
       users: [...users, data],
-      newUser: ""
+      newUser: "",
+      loading: false
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -48,10 +64,30 @@ export default class Main extends Component {
             returnKeyLabel="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
